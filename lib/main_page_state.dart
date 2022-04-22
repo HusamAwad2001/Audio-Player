5533793be111ecb31d305b1238bfd21574f584cb
wwwma <www.hosamawad2016@gmail.com>
2022-04-22 15:20:32 +0300

    upload project

---
+++ lib/main_page_state.dart
@@ -0,0 +1,137 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+class MainPageState extends StatefulWidget {
+  const MainPageState({Key? key}) : super(key: key);
+
+  @override
+  State<MainPageState> createState() => _MainPageStateState();
+}
+
+class _MainPageStateState extends State<MainPageState> {
+  final audioPlayer = AudioPlayer();
+  bool isPlaying = false;
+  Duration duration = Duration.zero;
+  Duration position = Duration.zero;
+
+  @override
+  void initState() {
+    setAudio();
+
+    super.initState();
+    audioPlayer.onPlayerStateChanged.listen((state) {
+      setState(() {
+        isPlaying = state == PlayerState.PLAYING;
+      });
+    });
+    audioPlayer.onDurationChanged.listen((newDuration) {
+      setState(() {
+        duration = newDuration;
+      });
+    });
+    audioPlayer.onAudioPositionChanged.listen((newPosition) {
+      position = newPosition;
+    });
+  }
+
+  Future setAudio() async {
+    audioPlayer.setReleaseMode(ReleaseMode.LOOP);
+
+    // final result = await FilePicker.platform.pickFiles();
+    // if (result != null) {
+    //   final file = File(result.files.single.path!);
+    //   audioPlayer.setUrl(file.path, isLocal: true);
+    // }
+    final player = AudioCache(prefix: 'assets/');
+    final url = await player.load('s.mp3');
+    audioPlayer.setUrl(url.path, isLocal: true);
+  }
+
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipRect(
+              child: Image.network(
+                'https://media.istockphoto.com/photos/blue-mosque-and-hagia-sophia-picture-id475460738?k=20&m=475460738&s=612x612&w=0&h=yJfvCrLKfRY7Ko-80-VOOTDfsK9-qWggdsJ2v98vo38=',
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: 350,
+              ),
+            ),
+            const SizedBox(height: 32),
+            const Text(
+              '💚 The Flutter Song',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 4),
+            const Text(
+              'Sarah Abs',
+              style: TextStyle(fontSize: 20),
+            ),
+            Slider(
+              min: 0,
+              max: duration.inSeconds.toDouble(),
+              value: position.inSeconds.toDouble(),
+              onChanged: (value) async {
+                final position = Duration(seconds: value.toInt());
+                await audioPlayer.seek(position);
+                await audioPlayer.resume();
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    formatTime(position),
+                  ),
+                  Text(
+                    formatTime(duration - position),
+                  ),
+                ],
+              ),
+            ),
+            CircleAvatar(
+              radius: 35,
+              child: IconButton(
+                icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+                iconSize: 50,
+                onPressed: () async {
+                  if (isPlaying) {
+                    await audioPlayer.pause();
+                  } else {
+                    await audioPlayer.resume();
+                  }
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  String formatTime(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = twoDigits(duration.inHours);
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+
+    return [if (duration.inHours > 0) hours, minutes, seconds].join(':');
+  }
+}
